ae32dcc7feb01018dc3639661bd256adb1ee0c74
beginner874 <davideyu4@gmail.com>
2026-06-07 20:01:07 +0000

    codice python 1

---
+++ ADS1ver1input.py
@@ -0,0 +1,31 @@
+card=0
+numeroB=0
+n=int(input("inserisci il numero massimo di elementi dell'insieme: "))
+insieme=[]
+for i in range(n):
+    elem =input(f"inserisci elemento (solo singolo carattere alfanumerico) {i + 1}: ")[0]
+    gia_presente=elem in insieme
+    if not gia_presente:
+        insieme.append(elem)
+        card+=1
+    else:
+        print("l'elemento è già presente nell'insieme")
+array=[0] * card
+if card > 0:
+    array[0]=1
+for i in range(1, card + 1):
+    print(array[i - 1], end=" ")
+print()
+for i in range(1, card):
+    temp=array[0]
+    for k in range(1, card):
+        temp1=temp
+        temp=array[k]
+        array[k]=array[k] * (k + 1) + temp1
+    for j in range(card):
+        print(array[j], end=" ")
+    print()
+for i in range(card):
+    numeroB += array[i]
+print()
+print(f"il numero di partizioni possibile sono: {numeroB}")
